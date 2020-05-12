4db711c1301bef7a70c8ee7faa4cb31b68b56f66
aj112358 <33339174+aj112358@users.noreply.github.com>
2020-05-11 23:08:26 -0600

    Update NIST_proto_AJ_Edit.py
    Made some more improvements after our meeting on that day.

--- NIST_proto_AJ_Edit.py
+++ NIST_proto_AJ_Edit.py
@@ -1,13 +1,19 @@
-##### CHANGES MADE BY AJ #####
+##### CHANGES MADE BY AJ ON MON, MAY 11 #####
+
+#                VERY IMPORTANT                    #
+### Need to use the 'nist_proto_AJ_Edit.kv' file!!!!
 
 ### Needed to change "textinput" table name to "text_input"
 ### Added more fields to my tables (not used in this python file)
-### Did not make any changes to the associated 'nist_proto.kv' file
+### Fixed issue with MC question creation (input boxes are now empty
+####### after question submission + goes back to select screen)
+### Got the slider questions to appear in patient windows
+### Got the MC answers to show up in patient windows
+### Included option for researcher to input slider start and increment
+### Got physical slider to work in patient window
+### Got it to show the slider value
 
-
-
-
-##### END OF CHANGES #####
+##### END OF CHANGES FROM AJ #####
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -18,6 +24,7 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
+from kivy.uix.slider import Slider
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -51,7 +58,7 @@ mydb = mysql.connector.connect(
     
     
 # this loads the cutomization kv file
-kv = Builder.load_file("nist_proto.kv") 
+kv = Builder.load_file("nist_proto_AJ_Edit.kv") 
     
     
 
@@ -113,6 +120,8 @@ class ReWindow(Screen):
         show_question_num()
         
 ### Window for researcher
+### This code creates a MC question and
+### puts it into the SQL database
 class MultWindow(Screen):
     question = ObjectProperty(None)
     option1 = ObjectProperty(None)
@@ -137,6 +146,11 @@ class MultWindow(Screen):
             val =(q, op1, op2, op3, op4)
             mycursor.execute(sql, val)
             mydb.commit()
+        self.question.text = ""
+        self.option1.text = ""
+        self.option2.text = ""
+        self.option3.text = ""
+        self.option4.text = ""
 
 def mult_duplicate(question):
     mycursor = mydb.cursor()
@@ -186,13 +200,19 @@ def show_question_num():
                 size_hint = (None, None), size = (400, 400))
     pop.open()
 
+### This code creates a slider question and puts it into SQL database
 class SliderWindow(Screen):
     question = ObjectProperty(None)
+    start = ObjectProperty(None)
     limit = ObjectProperty(None)
+    increment = ObjectProperty(None)
 
     def submit_Btn(self):
         q = self.question.text
+        strt = self.start.text
         lim = self.limit.text
+        inc = self.increment.text
+        
         if q == "" or lim == "":
             invalidInput()
         elif check_num(lim):
@@ -200,12 +220,16 @@ class SliderWindow(Screen):
         elif mult_duplicate(q):
             duplicate_warning()
         else:
-            print(int(lim))
             mycursor = mydb.cursor()
-            sql = "INSERT INTO slide (question,slide_limit) VALUES (%s, %s)"
-            val =(q, int(lim))
+            sql = "INSERT INTO slide (question, slide_limit, slide_start, slide_increment) VALUES (%s, %s, %s, %s)"
+            val = (q, int(lim), int(strt), int(inc))
             mycursor.execute(sql, val)
-            mydb.commit()            
+            mydb.commit() 
+                
+        self.question.text = ""
+        self.start.text = ""
+        self.limit.text = ""
+        self.increment.text = ""
 
 def check_num(st):
     for x in st:
@@ -259,8 +283,18 @@ class ThirdWindow(Screen):
 class WindowManager (ScreenManager):
     pass
 
+
+### This code creates a window on the PATIENT interface
+### for each MC question that is in the database
 class MultTemplate (Screen):
-    mult_question = StringProperty() 
+    mult_question = StringProperty()
+    MC_1 = StringProperty()
+    MC_2 = StringProperty()
+    MC_3 = StringProperty()
+    MC_4 = StringProperty()
+    
+    #### mylist = [mult_question, MC_1, MC_2, MC_3, MC_4]
+
     
     # this allows the kv label text to be changed via python
     def __init__(self, **kwargs):
@@ -273,19 +307,40 @@ class MultTemplate (Screen):
 
         mycursor = mydb.cursor()
         
-        ###print("This is the value of mult_num: %d" % mult_num)
+        # MC_FIELDS = ["question", 'option1', 'option2', 'option3', 'option4']
         
-        sql = "SELECT question FROM mult_choice WHERE mc_id = %d" % mult_num  
-        # * will give the entire row, specify a field after SELECT if just want one column 
-         
-        mycursor.execute(sql)
+        # for i in range(0,5):
+            # sql = "SELECT %s FROM mult_choice WHERE mc_id = %d" % (MC_FIELDS[i], mult_num)
+            # mycursor.execute(sql)
+            # myresult = mycursor.fetchall()
+            # self.mylist[i]= (myresult[0])[0]
+        
+        sql_question = "SELECT question FROM mult_choice WHERE mc_id = %d" % mult_num
+        mycursor.execute(sql_question)
+        myresult = mycursor.fetchall()        
+        self.mult_question = (myresult[0])[0]        
+        
+        sql_option1 = "SELECT option1 FROM mult_choice WHERE mc_id = %d" % mult_num
+        mycursor.execute(sql_option1)
         myresult = mycursor.fetchall() 
-         
-        #print((myresult[0])[0])    
-                                                      
-        # myresult produces the question as [(question)]
-        self.mult_question = (myresult[0])[0]
+        self.MC_1 = (myresult[0])[0] # myresult produces the question as [(question)]
     
+        sql_option2 = "SELECT option2 FROM mult_choice WHERE mc_id = %d" % mult_num
+        mycursor.execute(sql_option2)
+        myresult = mycursor.fetchall() 
+        self.MC_2 = (myresult[0])[0]   
+   
+        sql_option3 = "SELECT option3 FROM mult_choice WHERE mc_id = %d" % mult_num
+        mycursor.execute(sql_option3)
+        myresult = mycursor.fetchall() 
+        self.MC_3 = (myresult[0])[0]  
+
+        sql_option4 = "SELECT option4 FROM mult_choice WHERE mc_id = %d" % mult_num
+        mycursor.execute(sql_option4)
+        myresult = mycursor.fetchall() 
+        self.MC_4 = (myresult[0])[0]
+
+
     def next_Btn(self):
         if len (quest_str_lst) == 1:
             pass
@@ -294,8 +349,47 @@ class MultTemplate (Screen):
             next_q = quest_str_lst[0]
             sm.current = next_q
             sm.transition.diretion = "left"
-        
+       
+       
+### This code creates a window on the PATIENT interface
+### for each SLIDER question that is in the database
 class SlideTemplate(Screen):
+    
+    slider_question = StringProperty() 
+    start = NumericProperty()
+    limit = NumericProperty()
+    increment = NumericProperty()
+    
+    # this allows the kv label text to be changed via python
+    def __init__(self, **kwargs):
+        super(SlideTemplate, self).__init__(**kwargs)
+        self.question = ObjectProperty(None)
+        #self.answer1 = ObjectProperty(None)
+
+        mycursor = mydb.cursor()
+                
+        sql = "SELECT question FROM slide WHERE slide_id = %d" % slide_num  
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall() 
+        self.slider_question = (myresult[0])[0]
+        
+        sql = "SELECT slide_start FROM slide WHERE slide_id = %d" % slide_num  
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall() 
+        self.start = (myresult[0])[0]
+        
+        sql = "SELECT slide_increment FROM slide WHERE slide_id = %d" % slide_num  
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall() 
+        self.increment = (myresult[0])[0]
+                
+        sql = "SELECT slide_limit FROM slide WHERE slide_id = %d" % slide_num  
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall() 
+        self.limit = (myresult[0])[0]
+  
+    
+    
     def next_Btn(self):
         if len (quest_str_lst) == 1:
             pass
@@ -304,7 +398,8 @@ class SlideTemplate(Screen):
             next_q = quest_str_lst[0]
             sm.current = next_q
             sm.transition.direction = "left"
-            
+
+
 class TextTemplate(Screen):
     def next_Btn(self):
         if len (quest_str_lst) == 1:
@@ -347,7 +442,7 @@ screens = [SelectWindow(name = 'home'), LogWindow(name = 'login'),
 
 
 
-### Code to allow for any number of questions
+### Code to allow for any number of questions of any type
 mycursor = mydb.cursor()
 
 mycursor.execute("SELECT * FROM mult_choice")
@@ -369,7 +464,6 @@ print("The total number of Text questions is: %d" % NUM_OF_TEXT)
 ### End of that piece of code
 
 
-# this will be defined by rowcount of each table
 mult_num = 1
 mult_list = []
 slide_num = 1
@@ -392,7 +486,7 @@ while slide_num < NUM_OF_SLIDER+1:
     slide_list.append(screen)
     slide_num += 1
 
-while text_num < NUM_OF_TEXT+1 > 0:
+while text_num < NUM_OF_TEXT+1:
     screen = TextTemplate(name = 'text %d' % text_num)
     quest_str_lst.append("text %d" % text_num)
     text_list.append(screen)
